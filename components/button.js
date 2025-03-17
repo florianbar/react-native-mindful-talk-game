949ecb77b11a038c8a4193972d82d1df1967ddc0
Florian Bär <florianbar01@gmail.com>
2025-03-17 12:18:22 +0200

    improve button styling

--- components/button.js
+++ components/button.js
@@ -1,33 +1,44 @@
-import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { StyleSheet, Text, View, Pressable, Platform } from "react-native";
 
 export default function Button({ children, onPress }) {
-    return (
-        <View style={styles.button}>
-            <Pressable
-                android_ripple={{ color: '#45cdfe' }} // android only
-                style={({ pressed }) => pressed && styles.pressedButton} // cross-platform
-                onPress={onPress}
-            >
-                <Text style={styles.text}>{children}</Text>
-            </Pressable>
-        </View>
-    );
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        android_ripple={{ color: "#45cdfe" }} // android only
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressedButton]
+            : styles.buttonInnerContainer
+        } // cross-platform
+        onPress={onPress}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    button: {
-        backgroundColor: '#45a2fe',
-        borderRadius: 100,
-    },
-    pressedButton: {
-        opacity: 0.5,
-    },
-    text: {
-        paddingHorizontal: 24,
-        paddingVertical: 16,
-        fontSize: 18,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        color: 'white',
-    },
+  buttonOuterContainer: {
+    borderRadius: 100,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#45a2fe",
+    paddingHorizontal: 24,
+    paddingVertical: 16,
+  },
+  pressedButton: {
+    ...Platform.select({
+      ios: {
+        opacity: 0.75,
+      },
+    }),
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "white",
+  },
 });
